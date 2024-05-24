0ea9fe0a70fe9c485a0b4063bf125da0ec24b254
rodrigosiuves7 <rodrigo.ramires.ferraz@escola.pr.gov.br>
2024-05-24 09:47:07 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ console.log(botoes);
 for(let i = 0; i < botoes.length ; i++){
     botoes[i].onclick = function(){
 for (let j = 0 ; j < botoes.length ; j++){
-    botoes[j].classlist.remove("ativo")
+    botoes[j].classList.remove("ativo")
 }
 botoes[i].classList.add("ativo")
     }    
