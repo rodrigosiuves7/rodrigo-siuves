7fa8309b2133bf9743c8ffe49f49789aa838eeed
rodrigosiuves7 <rodrigo.ramires.ferraz@escola.pr.gov.br>
2024-07-25 08:06:24 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 const botoes = document.querySelectorAll(".botao");
 const conteudo = document.querySelectorAll(".aba-conteudo");
 const tempo = document.querySelectorAll(".contador");
-const tempoObjetiv1 = new Date(2024,11,31,59,59);
+const tempoObjetivo1 = new Date(2024,11,31,59,59);
 const tempoObjetivo2 = new Date(2024,10,28,59,59);
 const tempoObjetivo3 = new Date(2024,9,29,59,59);
 const tempoObjetivo4 = new Date(2024,8,27,59,59);
@@ -21,13 +21,14 @@ horas = Math.floor(horas)
 dias = Math.floor(dias)
 segundos = segundos%60
 minutos = minutos%60
-horas = horas%60
+horas = horas%24
+if (segundos>=0){
  return `Faltam ${horas} dias, ${horas} horas, ${minutos} minutos e ${segundos} segundos`
 }
-tempo[0].textcontent = calculaTempo(tempoObjetivo1);
-tempo[1].textcontent = calculaTempo(tempoObjetivo2);
-tempo[2].textcontent = calculaTempo(tempoObjetivo3);
-tempo[3].textcontent = calculaTempo(tempoObjetivo4);
+tempo[0].textContent = calculaTempo(tempoObjetivo1);
+tempo[1].textContent = calculaTempo(tempoObjetivo2);
+tempo[2].textContent = calculaTempo(tempoObjetivo3);
+tempo[3].textContent = calculaTempo(tempoObjetivo4);
 
 
 for(let i = 0; i < botoes.length ; i++){
